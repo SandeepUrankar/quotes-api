bab420f0ca9d3bfc24ce90f84ccda15236977fd9
SandeepUrankar <sandeepurankar.98@gmail.com>
2023-08-27 18:06:53 +0530

    Add random quote button.

--- client/app.js
+++ client/app.js
@@ -1,20 +1,26 @@
 "use strict";
 const quoteElement = document.querySelector(".container .quote");
 const authorElement = document.querySelector(".container .author");
+const getQuoteButton = document.querySelector(".container .quote__btn");
 const API_URL = "https://quote-backend-2jsz.onrender.com/api/v1/quotes";
 async function getQuote() {
-  const response = await fetch(API_URL);
-  const quote = await response.json();
-  return quote;
+    const response = await fetch(API_URL);
+    const quote = await response.json();
+    return quote;
 }
-getQuote()
-  .then((quote) => {
-    console.log(quote);
-    if (quoteElement != null && authorElement != null) {
-      quoteElement.innerHTML = quote.quote;
-      authorElement.textContent = quote.author;
+async function display() {
+    try {
+        const quote = await getQuote();
+        if (quoteElement != null && authorElement != null) {
+            quoteElement.innerHTML = quote.quote;
+            authorElement.textContent = quote.author;
+        }
+        console.log(quote);
     }
-  })
-  .catch((err) => {
-    console.log("Error while fetching data: ", err);
-  });
+    catch (error) {
+        console.error("Error while fetching data : ", error);
+    }
+}
+getQuoteButton?.addEventListener("click", () => {
+    display();
+});
--- client/app.ts
+++ client/app.ts
@@ -2,6 +2,7 @@ const quoteElement = document.querySelector(
   ".container .quote"
 ) as HTMLParagraphElement | null;
 const authorElement = document.querySelector(".container .author");
+const getQuoteButton = document.querySelector(".container .quote__btn");
 
 const API_URL: string = "https://quote-backend-2jsz.onrender.com/api/v1/quotes";
 
@@ -11,17 +12,21 @@ async function getQuote() {
   return quote;
 }
 
-getQuote()
-  .then((quote: Quote) => {
-    console.log(quote);
+async function display() {
+  try {
+    const quote: Quote = await getQuote();
     if (quoteElement != null && authorElement != null) {
       quoteElement.innerHTML = quote.quote;
       authorElement.textContent = quote.author;
     }
-  })
-  .catch((err) => {
-    console.log("Error while fetching data: ", err);
-  });
+    console.log(quote);
+  } catch (error) {
+    console.error("Error while fetching data : ", error);
+  }
+}
+getQuoteButton?.addEventListener("click", () => {
+  display();
+});
 
 interface Quote {
   id: number;
